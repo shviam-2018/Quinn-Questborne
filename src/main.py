1df76708926d9114098256cdb4e5edfc86b71510
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2024-01-20 22:20:27 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,37 +1,22 @@
 import pygame
+from pygame.locals import *
 
 pygame.init()
 
-screen_width = 800
+screen_Width = 800
 screen_Height = 600
 
-screen = pygame.display.set_mode((screen_width, screen_Height))
+screen = pygame.display.set_mode((screen_Width, screen_Height))
 pygame.display.set_caption("Quinn Questborne")
-player = pygame.Rect((300, 200, 50, 50))
+
+#load image
 
 run = True
-while run:
-    screen.fill((0, 0, 0))
-    
-    pygame.draw.rect(screen, (255, 0, 0), player)
-    
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-1, 0)
+while run: 
         
-    elif key[pygame.K_d] == True:
-            player.move_ip(1, 0)
-            
-    elif key[pygame.K_w] == True:
-            player.move_ip(0, -1)
-            
-    elif key[pygame.K_s] == True:
-            player.move_ip(0, 1)
-        
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    
-    pygame.display.update()
-    
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        run = False
+                        
+        pygame.display.update()
 pygame.quit()
